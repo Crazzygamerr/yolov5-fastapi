9b138623ad5357108caff0b8e74bec70e06f0a15
Rishi Balamurugan <rishi.b2372@gmail.com>
2022-07-13 13:27:06 +0530

    feat: Crop images

--- main.py
+++ main.py
@@ -1,70 +1,108 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, Request
+# import logging
 from segmentation import get_yolov5, get_image_from_bytes
 from starlette.responses import Response
 import io
 from PIL import Image
 import json
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+# Command to start server
+# uvicorn main:app --reload --host 0.0.0.0 --port 8000
+
+class ImageCrop(BaseModel):
+		unit: str
+		x: float
+		y: float
+		width: float
+		height: float
 
 model = get_yolov5()
 
 app = FastAPI(
-    title="Custom YOLOV5 Machine Learning API",
-    description="""Obtain object value out of image
-                    and return image and json result""",
-    version="0.0.1",
+		title="Custom YOLOV5 Machine Learning API",
+		description="""Obtain object value out of image
+										and return image and json result""",
+		version="0.0.1",
 )
 
+# logger = logging.getLogger("api")
+# logger.setLevel(logging.DEBUG)
+
 origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "*"
+		"*"
 ]
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+		CORSMiddleware,
+		allow_origins=origins,
+		allow_credentials=True,
+		allow_methods=["*"],
+		allow_headers=["*"],
 )
 
+# @app.get('/')
+# def test():
+# 	return detect_food_return_json_result(file=File(""))
 
-@app.get('/notify/v1/health')
+
+@app.get('/health')
 def get_health():
-    """
-    Usage on K8S
-    readinessProbe:
-        httpGet:
-            path: /notify/v1/health
-            port: 80
-    livenessProbe:
-        httpGet:
-            path: /notify/v1/health
-            port: 80
-    :return:
-        dict(msg='OK')
-    """
-    return dict(msg='OK')
+		"""
+		Usage on K8S
+		readinessProbe:
+				httpGet:
+						path: /notify/v1/health
+						port: 80
+		livenessProbe:
+				httpGet:
+						path: /notify/v1/health
+						port: 80
+		:return:
+				dict(msg='OK')
+		"""
+		return dict(msg='OK')
 
 
 @app.post("/object-to-json")
-async def detect_food_return_json_result(file: bytes = File(...)):
-    input_image = get_image_from_bytes(file)
-    results = model(input_image)
-    detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
-    detect_res = json.loads(detect_res)
-    return {"result": detect_res}
+async def detect_food_return_json_result(
+	request: Request,
+	file: bytes = File(...),
+	):
+		form = await request.form()
+		crop = None
+		if "crop" in form.keys() and form["crop"] is not None:
+			crop = ImageCrop(**(json.loads(form["crop"])))
+		input_image = get_image_from_bytes(
+			file, 
+			crop=crop
+			)
+			
+		results = model(input_image)
+		detect_res = results.pandas().xyxy[0].to_json(orient="records")  # JSON img1 predictions
+		detect_res = json.loads(detect_res)
+		return {"result": detect_res}
 
 
 @app.post("/object-to-img")
-async def detect_food_return_base64_img(file: bytes = File(...)):
-    input_image = get_image_from_bytes(file)
-    results = model(input_image)
-    results.render()  # updates results.imgs with boxes and labels
-    for img in results.imgs:
-        bytes_io = io.BytesIO()
-        img_base64 = Image.fromarray(img)
-        img_base64.save(bytes_io, format="jpeg")
-    return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
+async def detect_food_return_base64_img(
+	request: Request,
+	file: bytes = File(...),
+	):
+		form = await request.form()
+		crop = None
+		if "crop" in form.keys() and form["crop"] is not None:
+			crop = ImageCrop(**(json.loads(form["crop"])))
+		input_image = get_image_from_bytes(
+			file, 
+			crop=crop
+			)
+			
+		results = model(input_image)
+		results.render()  # updates results.imgs with boxes and labels
+		for img in results.imgs:
+				bytes_io = io.BytesIO()
+				img_base64 = Image.fromarray(img)
+				img_base64.save(bytes_io, format="jpeg")
+		return Response(content=bytes_io.getvalue(), media_type="image/jpeg")
--- segmentation.py
+++ segmentation.py
@@ -1,23 +1,44 @@
 import torch
 from PIL import Image
 import io
+from pydantic import BaseModel
 
+class ImageCrop(BaseModel):
+		unit: str
+		x: float
+		y: float
+		width: float
+		height: float
 
 def get_yolov5():
-    # local best.pt
-    model = torch.hub.load('./yolov5', 'custom', path='./model/best.pt', source='local')  # local repo
-    model.conf = 0.5
-    return model
+		# local best.pt
+		model = torch.hub.load('./yolov5', 'custom', path='./model/best.pt', source='local')  # local repo
+		model.conf = 0.25
+		return model
 
 
-def get_image_from_bytes(binary_image, max_size=1024):
-    input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
-    width, height = input_image.size
-    resize_factor = min(max_size / width, max_size / height)
-    resized_image = input_image.resize(
-        (
-            int(input_image.width * resize_factor),
-            int(input_image.height * resize_factor),
-        )
-    )
-    return resized_image
+def get_image_from_bytes(
+	binary_image, 
+	# max_size=1024, 
+	crop: ImageCrop = None,
+	):
+		input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+		width, height = input_image.size
+		# resize_factor = min(max_size / width, max_size / height)
+		# resized_image = input_image.resize(
+		#     (
+		#         int(input_image.width * resize_factor),
+		#         int(input_image.height * resize_factor),
+		#     )
+		# )
+		if crop is not None:
+			print(crop)
+			resized_image = input_image.crop((
+				crop.x, 
+				crop.y, 
+				crop.x + crop.width, 
+				crop.y + crop.height
+				))
+		else:
+			resized_image = input_image
+		return resized_image
